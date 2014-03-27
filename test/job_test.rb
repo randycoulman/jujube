3ec5cccec86ba726cefb3c2433278d28e609979c
Randy Coulman <rcoulman@gmail.com>
2014-03-26 17:27:14 -0700

    Make YAML matching more forgiving.
    There needs to be a more robust way to write these tests.

--- test/job_test.rb
+++ test/job_test.rb
@@ -42,12 +42,12 @@ class JobTest < Minitest::Test
 
   def test_includes_single_item_section
     @job.triggers << "TRIGGER"
-    assert_yaml_matches("triggers:\n  - TRIGGER")
+    assert_yaml_matches("triggers:\s*\n  - TRIGGER")
   end
 
   def test_includes_multi_item_section
     @job.wrappers << "FIRST" << "SECOND"
-    assert_yaml_matches("wrappers:\n  - FIRST\n  - SECOND")
+    assert_yaml_matches("wrappers:\s*\n  - FIRST\n  - SECOND")
   end
 
   def test_infers_matrix_project_type_when_axes_specified
@@ -57,7 +57,7 @@ class JobTest < Minitest::Test
 
   def test_renames_scms_to_scm
     @job.scm << "SCM"
-    assert_yaml_matches("scm:\n  - SCM")
+    assert_yaml_matches("scm:\s*\n  - SCM")
   end
 
   def test_self_registers_with_active_registry
@@ -73,7 +73,7 @@ class JobTest < Minitest::Test
   private
 
   def assert_yaml_matches(snippet)
-    expected = %r{^job:$.*^  #{snippet}$}m
+    expected = %r{^job:\s*$.*^  #{snippet}$}m
     assert_match(expected, @job.to_yaml)
   end
 end
