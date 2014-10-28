b17aa8bca942047cbd790c954b8dd1c0b0525305
Travis Dempsey <dempsey.travis@gmail.com>
2014-10-28 14:15:05 -0500

    added reverse trigger test

--- test/components/triggers_test.rb
+++ test/components/triggers_test.rb
@@ -3,6 +3,12 @@ require_relative "../test_helper"
 class TriggersTest < Minitest::Test
   include Jujube::Components
 
+  def test_reverse
+    expected = { 'reverse' => { 'jobs' => 'foo,bar', 'result' => 'success' } }
+    actual = reverse(watch_projects: %w(foo bar), trigger_on: 'success')
+    assert_equal(expected, actual)
+  end
+
   def test_pollscm
     assert_equal({"pollscm" => "INTERVAL"}, pollscm("INTERVAL"))
   end
