59895b50e770af48b7cff8263c8ef8b49bb20c22
Randy Coulman <rcoulman@gmail.com>
2016-06-08 07:18:40 -0700

    Add test for string parameter
    Code change from #3

--- test/components/parameters_test.rb
+++ test/components/parameters_test.rb
@@ -3,6 +3,11 @@ require_relative "../test_helper"
 class ParametersTest < Minitest::Test
   include Jujube::Components
 
+  def test_string
+    expected = {"string" => {"name" => "NAME", "default" => "DEFAULT"}}
+    assert_equal(expected, string(name: "NAME", default: "DEFAULT"))
+  end
+
   def test_validating_string
     expected = {"validating-string" => {"name" => "NAME", "regex" => "REGEX", "msg" => "MESSAGE" }}
     assert_equal(expected, validating_string(name: "NAME", regex: "REGEX", msg: "MESSAGE"))
