e8fd60b7d0a16d65710d9b1b88c67dc32e003d2e
Randy Coulman <rcoulman@gmail.com>
2016-06-08 07:10:24 -0700

    Support the validating-string parameter

--- lib/jujube/components/parameters.rb
+++ lib/jujube/components/parameters.rb
@@ -13,6 +13,15 @@ module Jujube
       # @param options [Hash] The configuration options for the component.
       # @return [Hash] The specification for the component.
       standard_component :string
+
+      # @!method validating_string(options = {})
+      # Specify a `validating-string` parameter for a job.
+      #
+      # See {http://docs.openstack.org/infra/jenkins-job-builder/parameters.html#parameters.validating-string}.
+      #
+      # @param options [Hash] The configuration options for the component.
+      # @return [Hash] The specification for the component.
+      standard_component :validating_string
     end
   end
 end
